0e3a7fa6554b57b2bbabf92a38b8853dc1515623
Andreas Brommund <andreas@brommund.se>
2018-02-01 18:19:38 +0100

    Update test

--- Server/main.py
+++ Server/main.py
@@ -2,9 +2,16 @@ from FiatShamir import FiatShamir
 
 
 def main():
-    fs = FiatShamir()
+    fs1 = FiatShamir(index=105)
+    print(fs1.new_user("U1", 13))
+    print(fs1.new_user("U2", 12))
+    print(fs1.new_user("U3", 10))
 
-    fs = FiatShamir(index=fs.index)
+    fs2 = FiatShamir()
+    print(fs2.new_user("U4", 14))
+    print(fs2.new_user("U5", 15))
+
+    print(fs1.new_user("U6", 16))
 
 
 if __name__ == "__main__":
