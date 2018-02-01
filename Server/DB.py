73a98dfa8ec4655c562a8298f2c69f32b418697e
Andreas Brommund <andreas@brommund.se>
2018-02-01 18:17:58 +0100

    Refactor DB script

--- Server/DB.py
+++ Server/DB.py
@@ -39,21 +39,16 @@ def __cursor(con):
     except psycopg2.DatabaseError as e:
         if con:
             con.close()
-
         print('Error %s' % e)
         sys.exit(1)
 
 
-def new_user(user_name, db):
-    sql_string = "INSERT INTO public.user (user_name) VALUES (%s)"
-
+def __execute(tasks, db):
     con = db.connect()
     cur = __cursor(con)
 
     try:
-        cur.execute(sql_string, (user_name,))
-
-        con.commit()
+        return_value = tasks(con, cur)
     except psycopg2.DatabaseError as e:
         if con:
             con.rollback()
@@ -65,50 +60,64 @@ def new_user(user_name, db):
             con.close()
         if cur:
             cur.close()
+
+    return return_value
+
+
+def new_user(user_name, key, fiat_shamir_id, db):
+    def execute(con, cur):
+        sql_string = "INSERT INTO public.user (user_name) VALUES (%s) RETURNING id"
+        cur.execute(sql_string, (user_name,))
+        user_index = cur.fetchone()['id']
+        sql_string = "INSERT INTO public.public_key (fiat_shamir_id, user_id, key) VALUES (%s,%s,%s)"
+        cur.execute(sql_string, (fiat_shamir_id, user_index, __int_to_bytes(key)))
+        con.commit()
+        return user_index
+
+    return __execute(execute, db)
 
 
 def new_fiat_shamir(key, db):
-    sql_string = "INSERT INTO public.fiat_shamir (public_key) VALUES (decode(%s,'hex')) RETURNING id"
-
-    con = db.connect()
-    cur = __cursor(con)
-
-    try:
-        cur.execute(sql_string, (key[2:],))  # Remove \0x from the hex string
+    def execute(con, cur):
+        sql_string = "INSERT INTO public.fiat_shamir (public_key) VALUES (%s) RETURNING id"
+        cur.execute(sql_string, (__int_to_bytes(key),))
         index = cur.fetchone()['id']
         con.commit()
-    except psycopg2.DatabaseError as e:
-        if con:
-            con.rollback()
-        print('Error %s' % e)
-        sys.exit(1)
+        return index
 
-    finally:
-        if con:
-            con.close()
-        if cur:
-            cur.close()
-
-    return index
+    return __execute(execute, db)
 
 
 def get_fiat_shamir(index, db):
-    sql_string = "SELECT public_key FROM fiat_shamir WHERE id = %s"
-
-    con = db.connect()
-    cur = __cursor(con)
-
-    try:
+    def execute(_, cur):
+        sql_string = "SELECT public_key FROM fiat_shamir WHERE id = %s"
         cur.execute(sql_string, (index,))
-        public_key = cur.fetchone()['public_key']
-    except psycopg2.DatabaseError as e:
-        print('Error %s' % e)
-        sys.exit(1)
+        res = cur.fetchone()
+        if res is not None:
+            return __int_from_bytes(res['public_key'])
+        else:
+            return None
 
-    finally:
-        if con:
-            con.close()
-        if cur:
-            cur.close()
+    return __execute(execute, db)
 
-    return public_key
+
+def public_key_exist(key, db):
+    def execute(_, cur):
+        """ 
+        We could add "AND public.public_key.fiat_shamir_id = %s". Because we only require that the key is unique in a
+        fiat_shamir instance. But the DB schema require that every row is unique for now
+        """
+        sql_string = "SELECT EXISTS(SELECT 1 FROM public.public_key WHERE  key = %s) AS exists"
+        cur.execute(sql_string, (__int_to_bytes(key),))
+        exist = cur.fetchone()['exists']
+        return exist
+
+    return __execute(execute, db)
+
+
+def __int_to_bytes(x):
+    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
+
+
+def __int_from_bytes(xbytes):
+    return int.from_bytes(xbytes, 'big')
