68059b3a61ac573552f6ea644d7ffa1a1bf2731e
Andreas Brommund <andreas@brommund.se>
2018-02-01 18:18:58 +0100

    Can now add a new user with a unique public key

--- Server/FiatShamir.py
+++ Server/FiatShamir.py
@@ -9,19 +9,38 @@ class FiatShamir:
         self.db = DatabaseConnection()
 
         if index:
-            self.index = index
+            self.id = index
             self.__load_from_db()
         else:
             self.__new_instance(key_size)
 
-        print(self.index)
+        print(self.id)
         print(self.n)
 
     def __load_from_db(self):
-        index = DB.get_fiat_shamir(self.index, self.db)
-        self.n = int.from_bytes(bytes(index), byteorder='big')
+        self.n = DB.get_fiat_shamir(self.id, self.db)
 
-    def __new_instance(self, key_size):
+    def __new_instance(self, key_size: int):
         p, q = crypt_math.generate_primes(key_size)
         self.n = p * q
-        self.index = DB.new_fiat_shamir(hex(self.n), self.db)
+        self.id = DB.new_fiat_shamir(self.n, self.db)
+
+    def new_user(self, user_name, user_public_key):
+
+        if not (1 <= user_public_key <= self.n - 1):
+            print("User_public_key need to fulfill: 1 <= user_public_key <= n-1")
+            return -1
+
+        d, _, _ = crypt_math.extended_euclidean_algorithm(user_public_key, self.n)
+        if d != 1:
+            print("gcd(user_public_key,n) need to be equal to 1.")
+            return -1
+
+        exist = DB.public_key_exist(user_public_key, self.db)
+        if exist:
+            print("The user_public_key is not unique ")
+            return -1
+
+        user_id = DB.new_user(user_name, user_public_key, self.id, self.db)
+
+        return user_id
